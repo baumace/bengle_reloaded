77a86bef9d595fa1177470a72e2ea8acd74b651d
Ace Baum <jaybaum.03@gmail.com>
2023-11-16 22:38:55 -0500

    created basic guess players and row structure

--- app/src/components/GuessedPlayers.js
+++ app/src/components/GuessedPlayers.js
@@ -1,7 +1,16 @@
+import Row from "./guess_player_components/Row";
+
 export default function GuessedPlayers() {
     return (
         <div class="bg-blue-600 mb-auto">
-            <p>Guessed players here!</p>
+            <Row header={true} />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Row />
         </div>
     );
 }
--- app/src/components/guess_player_components/Row.js
+++ app/src/components/guess_player_components/Row.js
@@ -1,7 +1,14 @@
-export default function Row() {
+export default function Row(header) {
     return (
         <div class="bg-blue-600 mb-auto">
-            <p>Row here!</p>
+            <span class="flex">
+                <div>Name</div>
+                <div>College</div>
+                <div>Position</div>
+                <div>Round</div>
+                <div>Pick #</div>
+                <div>Year</div>
+            </span>
         </div>
     );
 }
